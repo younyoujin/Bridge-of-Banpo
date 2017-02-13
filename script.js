1bd3cb561954b8490b6a25522a0a085205341eb8
jesterz91 <jesterz91@naver.com>
2017-02-13 12:18:53 +0900

    no use
    o

--- script.js
+++
@@ -1,8 +0,0 @@
-wbtn =document.getElementById('white_btn');
-wbtn.addEventListener('click', function(){
-  document.getElementById('target').className='white';
-});
-bbtn =document.getElementById('black_btn');
-bbtn.addEventListener('click', function(){
-  document.getElementById('target').className='black';
-});
